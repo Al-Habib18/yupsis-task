6f0598fcd151dd9ca9855911d293cff6af63e195
al_habib <ah18bd@gmail.com>
2025-05-30 14:06:01 +0600

    updated task

--- auth/prisma/migrations/20250529024534_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `status` on the `VerificationCode` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "VerificationCode" DROP COLUMN "status";
--- auth/src/config/default.ts
+++
@@ -1,6 +0,0 @@
-/** @format */
-
-const default_email_sender =
-    process.env.DEFAULT_EMAIL_SENDER_EMAIL ?? "mX2P5@example.com";
-
-export { default_email_sender };
